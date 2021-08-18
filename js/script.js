feb1456a3a3d3f7919cf27c603fbfdaaa35cd3d2
Andres Vidoza <andresvidoza@gmail.com>
2021-08-17 20:44:08 -0400

    d

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-console.log("hello")
+let password__header__loginBtn = document.querySelector(".password__header__login");
+let password__login__closeBtn = document.querySelector(".password__login__close");
+let password__login = document.querySelector(".password__login");
+
+password__header__loginBtn.addEventListener('click', function(){
+    if(password__login.classList.contains("close")){
+        password__login.classList.add("open")
+        password__login.classList.remove("close")
+    }else{
+        password__login.classList.add("close")
+        password__login.classList.remove("open")
+    }
+})
+
+password__login__closeBtn.addEventListener('click', function(){
+    if(password__login.classList.contains("close")){
+        password__login.classList.add("open")
+        password__login.classList.remove("close")
+    }else{
+        password__login.classList.add("close")
+        password__login.classList.remove("open")
+    }
+})
